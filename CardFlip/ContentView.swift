4d9d7834cb3272a2cc75688cb80e2e4bb75945ed
Damon Gonzalez <damon.r.gonzalez@gmail.com>
2024-11-12 11:36:36 -0800

    initial commit

--- CardFlip/ContentView.swift
+++ CardFlip/ContentView.swift
@@ -8,17 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showView: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                ZStack {
+                    if showView {
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.red)
+                            .transition(.reverseFlip)
+                    }
+                    else {
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.blue)
+                            .transition(.flip)
+                    }
+                }
+                .frame(width: 200, height: 300)
+                .onTapGesture {
+                    withAnimation(.bouncy(duration: 2)){
+                        showView.toggle()
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+
+
+struct FlipTransition:ViewModifier, Animatable {
+    var progress:CGFloat = 0.0
+    var animatableData:CGFloat {
+        get { progress }
+        set { progress = newValue }
+    }
+    func body(content: Content) -> some View {
+        content
+            .opacity(progress < 0 ? (-progress < 0.5 ? 1:0):(progress < 0.5 ? 1:0))
+            .rotation3DEffect(
+                .init(degrees: progress * 180),
+                axis: (x: 0, y: 1, z: 0)
+            )
+    }
+}
+
+
+
+extension AnyTransition {
+    static let flip:AnyTransition = .modifier(
+        active: FlipTransition(progress: -1),
+        identity: FlipTransition()
+    )
+    
+    static let reverseFlip:AnyTransition = .modifier(
+        active: FlipTransition(progress: 1),
+        identity: FlipTransition()
+    )
 }
